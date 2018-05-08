2b24e270a0bd47a774bb36ea6f3ac03f24b97769
jared <jared.jacobson.2000@gmail.com>
2018-05-08 17:20:08 -0600

    added game type enum to game interface

--- src/database/typings/game-interface.ts
+++ src/database/typings/game-interface.ts
@@ -1,7 +1,8 @@
 import { GamePlayer } from './game_player-interface';
+import { GameType } from './game_type-enum';
 
 export interface Game {
     name: string;
-    type: string;
+    type: GameType;
     players: GamePlayer[];
 }
