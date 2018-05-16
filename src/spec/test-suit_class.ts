569d4afd8edb47d2f1e9ead7069599516112a189
jared <jared.jacobson.2000@gmail.com>
2018-05-15 20:27:25 -0600

    finished implementing addCall and _objectExpect

--- src/spec/test-suit_class.ts
+++ src/spec/test-suit_class.ts
@@ -1,19 +1,34 @@
+import { colorLog, color } from "./color";
+
 export default class TestSuit {
-    protected _url: string | undefined;
-    protected _method: string | undefined;
-    addCall(callObject: object, expectedResponse: object): void {
+    protected _url: string;
+    protected _method: string;
+    constructor() {
+        this._url = '/';
+        this._method = 'GET';
+    }
+    public addCall(callObject: object, expectedResponse: object): void {
         const xhr = new XMLHttpRequest();
         let res: object;
         xhr.onreadystatechange = () => {
             if (xhr.status === xhr.DONE) {
                 res = xhr.response;
-                
+                this._objectExpect(res, expectedResponse);
             }
         }
+        xhr.open(this._method, this._url);
     }
-    private _expect(input: string) {
+    private _objectExpect(input: object, expected: object) {
+        let comparedObj = this._compareObj(input, expected);
+        colorLog(comparedObj.bool ? color.BgGreen : color.BgRed, `Expected: ${input} = ${expected}`);
+        comparedObj.same.forEach(key => {
+            colorLog(color.BgGreen, key);
+        });
+        comparedObj.diff.forEach(key => {
+            colorLog(color.BgRed, key);
+        })
     }
-    static _compareObj(x: object, y: object): {bool: boolean, same: string[], diff: string[]} {
+    private _compareObj(x: object, y: object): {bool: boolean, same: string[], diff: string[]} {
         let sameObj: {bool: boolean, same: string[], diff: string[]} = {
             bool: true,
             same: [],
