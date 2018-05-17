0caed75e8aa0f960382df5f1153b093235a65610
jared <jared.jacobson.2000@gmail.com>
2018-05-16 18:23:47 -0600

    made Player interface extend document

--- src/start/typings/game_player-interface.ts
+++ src/start/typings/game_player-interface.ts
@@ -1,7 +1,9 @@
 import { PlayerType } from './player_type-enum';
+import { Document } from 'mongoose';
 
-export interface GamePlayer {
+export interface GamePlayer extends Document {
     name: string;
     type: PlayerType;
     score: number;
+    currentTurn: boolean;
 }
