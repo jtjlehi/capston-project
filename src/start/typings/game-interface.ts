b69c2399a551ec80dfd19d2f5d65512574b89351
jared <jared.jacobson.2000@gmail.com>
2018-05-16 18:22:17 -0600

    made the game interface extend document and have 2 different kinds

--- src/start/typings/game-interface.ts
+++ src/start/typings/game-interface.ts
@@ -1,8 +1,15 @@
 import { GamePlayer } from './game_player-interface';
 import { GameType } from './game_type-enum';
+import { Document } from 'mongoose';
+import { ObjectID } from 'mongodb';
 
-export interface Game {
+export interface LocalGame extends Document {
     name: string;
-    type: GameType;
+    type: GameType.Local;
     players: GamePlayer[];
+}
+export interface OnlineGame extends Document {
+    name: string;
+    type: GameType.Online;
+    players: ObjectID[];
 }
