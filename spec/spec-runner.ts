c1a1462a33025f410263206bccafb69b01ac7733
jared <jared.jacobson.2000@gmail.com>
2018-05-19 16:07:58 -0600

    added an async function before my tests that cleared the db

--- spec/spec-runner.ts
+++ spec/spec-runner.ts
@@ -1,3 +1,14 @@
 import { AddGameTestSuit } from "./add-game";
+import { connect, connection, disconnect } from "mongoose";
+
+(async () => {
+    try {
+        await connect('mongodb://localhost/game');
+        await connection.dropDatabase();
+        await disconnect();
+    } catch(error) {
+        console.log(error.message);
+    }
+})()
 
 AddGameTestSuit.run();
