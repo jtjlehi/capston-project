e0dd87936a388851b16a9e8f9d73e6df1db9c803
jared <jared.jacobson.2000@gmail.com>
2018-05-19 23:56:17 -0600

    set up the base responses for the AddLocalPlayer.post

--- src/start/add-local-player.ts
+++ src/start/add-local-player.ts
@@ -1,12 +1,39 @@
 import * as express from 'express';
 
-import { LocalPlayerResponse } from "../typings/response-types";
+import { LocalPlayerResponse, ErrorMessage } from "../typings/response-types";
+import { GamePlayer } from '../typings/player-types';
+import { Player } from './player_class';
 
 export class AddLocalPlayer {
-    public static post = async (
+    public static async post (
         req: express.Request,
         res: express.Response
-    ): Promise<LocalPlayerResponse> => {
-        throw new Error('AddLocalPlayer.post() not implemented yet');
+    ): Promise<GamePlayer | ErrorMessage> {
+        let errorMessage: ErrorMessage;
+        if (!req.body.player || !req.body.player) {
+            errorMessage = {
+                errorCode: 406,
+                message: "you must specify a player and game"
+            }
+            res.status(406);
+            res.send(errorMessage);
+            return errorMessage;
+        } try {
+            let player: GamePlayer = new Player(req.body.player);
+            res.status(201);
+            res.send({
+                game: req.body.game,
+                player: player
+            });
+            return player;
+        } catch(err) {
+            errorMessage = {
+                errorCode: 406,
+                message: 'unknown error occurred'
+            }
+            res.status(406);
+            res.send(errorMessage);
+            return errorMessage;
+        }
     }
 }
